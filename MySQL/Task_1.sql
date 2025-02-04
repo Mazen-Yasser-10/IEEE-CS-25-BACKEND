b8311f93c0f675b3ff3080410c4b190a7fba8ec6
mazen <mazenmashal@gmail.com>
2025-02-04 09:12:14 +0200

    fixing line

--- MySQL/Task_1.sql
+++ MySQL/Task_1.sql
@@ -3,4 +3,4 @@ SELECT * FROM CITY;
 -- Second >> Query all columns for a city in CITY with the ID 1661.
 SELECT * FROM CITY WHERE ID = 1661;
 -- Third >> Query a list of CITY and STATE from the STATION table.
-SELECT CITY,STATE STATE FROM STATION;
+SELECT CITY,STATE FROM STATION;
